1f9da2c302ba040f37a1eac7d4bf9a809779c1ae
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2024-05-31 17:06:02 +0900

    コード削除

--- main.cpp
+++ main.cpp
@@ -15,9 +15,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Vector3 cameraTranslate{0.0f, 1.9f, -6.49f}; 
 Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
-Segment segment = {{-2.0f, -1.0f, 0.0f}, {3.0f, 2.0f, 2.0f}};
-Vector3 point = {-1.5f, 0.6f, 0.6f};
- Sphere sphere = {{0.0f, 0.0f, 0.0f}, 1.0f};
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -41,16 +39,6 @@ Vector3 point = {-1.5f, 0.6f, 0.6f};
         Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
         Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f); 
 
-        Vector3 closestPoint = ClosestPoint(point, segment);
-
-        Sphere pointSphere = {point, 0.01f};
-        Sphere closestPointSphere = {closestPoint, 0.01f};
-
-		  Vector3 start = Transform(Transform(segment.origir, viewProjectionMatrix), viewportMatrix);
-    Vector3 end = Transform(Transform(Add(segment.origir,segment.diff),viewProjectionMatrix), viewportMatrix);
-
-		Vector3 project = Project(Subtract(point, segment.origir), segment.diff);
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -58,16 +46,9 @@ Vector3 point = {-1.5f, 0.6f, 0.6f};
 		///
 		/// ↓描画処理ここから
 		///
-		  DrawGrid(viewProjectionMatrix, viewportMatrix);
-		    DrawSphere(pointSphere, viewProjectionMatrix, viewportMatrix, 0xFF0000FF);  // Red
-        DrawSphere(closestPointSphere, viewProjectionMatrix, viewportMatrix, 0x000000FF);  // Black
-		    Novice::DrawLine((int)start.x, (int)start.y,
-                     (int)end.x, (int)end.y, 0xFFFFFFFF);
+		
 		ImGui::Begin("Window");
-        ImGui::InputFloat3("Point", &point.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
-        ImGui::InputFloat3("Segment origin", &segment.origir.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
-        ImGui::InputFloat3("Segment diff", &segment.diff.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
-		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+     
 ImGui::End();
 		///
 		/// ↑描画処理ここまで
