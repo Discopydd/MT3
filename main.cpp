0e97492791cc282d47ac9eb99f28af3d88a87732
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2024-07-10 22:14:35 +0900

    03_01完了

--- main.cpp
+++ main.cpp
@@ -23,10 +23,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Vector3 viewTranslate = {0.0f, 0.0f, 0.0f};
     Vector3 cameraScale = {1.0f, 1.0f, 1.0f};
 
-    Vector3 controlPoints[3] = {
-        {-0.8f, 0.58f, 1.0f},
-        {1.76f, 1.0f, -0.3f},
-        {0.94f, -0.7f, 2.3f}
+   Vector3 translates[3] = {
+        {0.2f, 1.0f, 0.0f}, // 肩
+        {0.4f, 0.0f, 0.0f}, // 肘
+        {0.3f, 0.0f, 0.0f}  // 手
+    };
+
+    Vector3 rotates[3] = {
+        {0.0f, 0.0f, -6.8f}, // 肩
+        {0.0f, 0.0f, -1.4f}, // 肘
+        {0.0f, 0.0f, 0.0f}   // 手
+    };
+
+    Vector3 scales[3] = {
+        {1.0f, 1.0f, 1.0f}, // 肩
+        {1.0f, 1.0f, 1.0f}, // 肘
+        {1.0f, 1.0f, 1.0f}  // 手
     };
 
     bool isDraggingMiddle = false;
@@ -103,7 +115,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         Matrix4x4 viewProjectionMatrix = Multiply(combinedViewMatrix, projectionMatrix);
         Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 
-        uint32_t color = WHITE;
+        Matrix4x4 shoulderLocalMatrix = MakeAffineMatrix(scales[0], rotates[0], translates[0]);
+        Matrix4x4 shoulderWorldMatrix = shoulderLocalMatrix;
+
+        Matrix4x4 elbowLocalMatrix = MakeAffineMatrix(scales[1], rotates[1], translates[1]);
+        Matrix4x4 elbowWorldMatrix = Multiply(elbowLocalMatrix, shoulderWorldMatrix);
+
+        Matrix4x4 handLocalMatrix = MakeAffineMatrix(scales[2], rotates[2], translates[2]);
+        Matrix4x4 handWorldMatrix = Multiply(handLocalMatrix, elbowWorldMatrix);
+
+
+        Sphere shoulder = { Transform({0.0f, 0.0f, 0.0f}, shoulderWorldMatrix), 0.05f };
+        Sphere elbow = { Transform({0.0f, 0.0f, 0.0f}, elbowWorldMatrix), 0.05f };
+        Sphere hand = { Transform({0.0f, 0.0f, 0.0f}, handWorldMatrix), 0.05f };
+
+        
+
+       
         ///
         /// ↑更新処理ここまで
         ///
@@ -111,19 +139,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         ///
         /// ↓描画処理ここから
         ///
-
-        DrawBezier(controlPoints[0], controlPoints[1], controlPoints[2], viewProjectionMatrix, viewportMatrix, color);
-
-        for (int i = 0; i < 3; ++i) {
-            Sphere sphere = { controlPoints[i], 0.01f }; 
-            DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, BLACK);
-        }
-
+        DrawSphere(shoulder, viewProjectionMatrix, viewportMatrix, RED);
+        DrawSphere(elbow, viewProjectionMatrix, viewportMatrix, GREEN);
+        DrawSphere(hand, viewProjectionMatrix, viewportMatrix, BLUE);
+        // 肩-肘、肘-手の線を描画
+        Segment shoulderToElbow = {shoulder.center, Subtract(elbow.center, shoulder.center)};
+        Segment elbowToHand = {elbow.center, Subtract(hand.center, elbow.center)};
+        
+        DrawSegment(shoulderToElbow, viewProjectionMatrix, viewportMatrix, WHITE);
+        DrawSegment(elbowToHand, viewProjectionMatrix, viewportMatrix, WHITE);
         DrawGrid(viewProjectionMatrix, viewportMatrix);
         ImGui::Begin("Window");
-        ImGui::DragFloat3("Control Point 0", &controlPoints[0].x, 0.01f);
-        ImGui::DragFloat3("Control Point 1", &controlPoints[1].x, 0.01f);
-        ImGui::DragFloat3("Control Point 2", &controlPoints[2].x, 0.01f);
+        ImGui::DragFloat3("translates[0]", &translates[0].x, 0.01f);
+         ImGui::DragFloat3("rotates[0]", &rotates[0].x, 0.01f);
+           ImGui::DragFloat3("scales[0]", &scales[0].x, 0.01f);
+        ImGui::DragFloat3("translates[1]", &translates[1].x, 0.01f);
+         ImGui::DragFloat3("rotates[1]", &rotates[1].x, 0.01f);
+           ImGui::DragFloat3("scales[1]", &scales[1].x, 0.01f);
+        ImGui::DragFloat3("translates[2]", &translates[2].x, 0.01f);
+        ImGui::DragFloat3("rotates[2]", &rotates[2].x, 0.01f);
+        ImGui::DragFloat3("scales[2]", &scales[2].x, 0.01f);
         ImGui::End();
         ///
         /// ↑描画処理ここまで
