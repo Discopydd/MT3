98c37934ed426c18cd63aa5305241400ce322b28
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2024-07-31 14:59:25 +0900

    03_01完了

--- main.cpp
+++ main.cpp
@@ -24,16 +24,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Vector3 cameraScale = {1.0f, 1.0f, 1.0f};
 
   
-     Vector3 a = {0.2f, 1.0f, 0.0f};
-    Vector3 b = {2.4f, 3.1f, 1.2f};
-    Vector3 c = a + b;
-    Vector3 d = a - b;
-    Vector3 e = a * 2.4f;
-      Vector3 rotate = {0.4f, 1.43f, -0.8f};
-    Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-    Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-    Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-    Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+    Vector3 translates[3] = {
+        {0.2f, 1.0f, 0.0f}, // 肩
+        {0.4f, 0.0f, 0.0f}, // 肘
+        {0.3f, 0.0f, 0.0f}  // 手
+    };
+
+    Vector3 rotates[3] = {
+        {0.0f, 0.0f, -6.8f}, // 肩
+        {0.0f, 0.0f, -1.4f}, // 肘
+        {0.0f, 0.0f, 0.0f}   // 手
+    };
+
+    Vector3 scales[3] = {
+        {1.0f, 1.0f, 1.0f}, // 肩
+        {1.0f, 1.0f, 1.0f}, // 肘
+        {1.0f, 1.0f, 1.0f}  // 手
+    };
 
     // キー入力結果を受け取る箱
     char keys[256] = {0};
@@ -59,6 +66,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
         Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f); 
         
+         Matrix4x4 shoulderLocalMatrix = MakeAffineMatrix(scales[0], rotates[0], translates[0]);
+        Matrix4x4 shoulderWorldMatrix = shoulderLocalMatrix;
+
+        Matrix4x4 elbowLocalMatrix = MakeAffineMatrix(scales[1], rotates[1], translates[1]);
+        Matrix4x4 elbowWorldMatrix = Multiply(elbowLocalMatrix, shoulderWorldMatrix);
+
+        Matrix4x4 handLocalMatrix = MakeAffineMatrix(scales[2], rotates[2], translates[2]);
+        Matrix4x4 handWorldMatrix = Multiply(handLocalMatrix, elbowWorldMatrix);
+
+
+        Sphere shoulder = { Transform({0.0f, 0.0f, 0.0f}, shoulderWorldMatrix), 0.05f };
+        Sphere elbow = { Transform({0.0f, 0.0f, 0.0f}, elbowWorldMatrix), 0.05f };
+        Sphere hand = { Transform({0.0f, 0.0f, 0.0f}, handWorldMatrix), 0.05f };
 
        
         ///
@@ -68,17 +88,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         ///
         /// ↓描画処理ここから
         ///
-      
+       DrawSphere(shoulder, viewProjectionMatrix, viewportMatrix, RED);
+        DrawSphere(elbow, viewProjectionMatrix, viewportMatrix, GREEN);
+        DrawSphere(hand, viewProjectionMatrix, viewportMatrix, BLUE);
+        // 肩-肘、肘-手の線を描画
+        Segment shoulderToElbow = {shoulder.center, Subtract(elbow.center, shoulder.center)};
+        Segment elbowToHand = {elbow.center, Subtract(hand.center, elbow.center)};
+
+        DrawSegment(shoulderToElbow, viewProjectionMatrix, viewportMatrix, WHITE);
+        DrawSegment(elbowToHand, viewProjectionMatrix, viewportMatrix, WHITE);
         DrawGrid(viewProjectionMatrix, viewportMatrix);
         ImGui::Begin("Window");
-       ImGui::Text("c: %f, %f, %f", c.x, c.y, c.z);
-        ImGui::Text("d: %f, %f, %f", d.x, d.y, d.z);
-        ImGui::Text("e: %f, %f, %f", e.x, e.y, e.z);
-        ImGui::Text("matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f",
-                    rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
-                    rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
-                    rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
-                    rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+      ImGui::DragFloat3("translates[0]", &translates[0].x, 0.01f);
+         ImGui::DragFloat3("rotates[0]", &rotates[0].x, 0.01f);
+           ImGui::DragFloat3("scales[0]", &scales[0].x, 0.01f);
+        ImGui::DragFloat3("translates[1]", &translates[1].x, 0.01f);
+         ImGui::DragFloat3("rotates[1]", &rotates[1].x, 0.01f);
+           ImGui::DragFloat3("scales[1]", &scales[1].x, 0.01f);
+        ImGui::DragFloat3("translates[2]", &translates[2].x, 0.01f);
+        ImGui::DragFloat3("rotates[2]", &rotates[2].x, 0.01f);
+        ImGui::DragFloat3("scales[2]", &scales[2].x, 0.01f);
         ImGui::End();
         ///
         /// ↑描画処理ここまで
